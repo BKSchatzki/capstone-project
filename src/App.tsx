ee222007c39040fb3c8a4a51f5eb8d4993373892
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2024-05-20 23:38:00 -0700

    Configure color theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="bg-primary">App</div>
+      <div className="bg-seconary">App</div>
+      <div className="bg-accent">App</div>
+      <div className="bg-base-100">App</div>
+      <div className="bg-base-200">App</div>
+      <div className="bg-neutral">App</div>
+    </>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: ['daisyui'],
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        lemon: {
+          primary: '#4a5e57',
+          secondary: 'fef4eb',
+          accent: 'f3cf42',
+          'base-100': '#ffffff',
+          'base-200': '#ededee',
+          neutral: '#333333',
+        },
+      },
+    ],
+  },
 };
